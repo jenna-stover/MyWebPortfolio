6a39c85f46306ce2d899e8ca73fe21d7c9e8cc36
jenna-stover.github.io <jenna.stover810@gmail.com>
2024-11-07 14:25:21 -0500

    committing changes

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section className="section">
+        <header className="header">
+          <h1>Jenna Stover</h1>
+          <p>Frontend Web Developer and Designer</p>
+        </header>
+        <div className="buttons">
+          <h3 id="project-button">PROJECTS {'>>'}</h3>
+          <h3 id="contact-button">CONTACT ME {'>>'}</h3>
+        </div>
+      </section>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ my-portfolio/src/index.js
@@ -0,0 +1,18 @@
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import './index.css';
+
+
+export default function Main() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Main />);
