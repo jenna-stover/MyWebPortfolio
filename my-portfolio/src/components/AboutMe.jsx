72d90e36b4fe645994e71a8c30fdb4cc1af22057
jenna-stover.github.io <jenna.stover810@gmail.com>
2024-12-12 10:57:51 -0500

    stashing changes

--- my-portfolio/src/components/AboutMe.jsx
+++ my-portfolio/src/components/AboutMe.jsx
@@ -1,7 +1,20 @@
 import "../styles/AboutMe.css";
+import React, { useState, useEffect } from 'react';
 import profilePic from '../assets/profile-pic.png';
 
 const AboutMe = () => {
+  const [step, setStep] = useState(0);
+
+  useEffect(() => {
+    if (step < 4) { 
+      const timeoutDuration = step === 0 ? 1000 : 1500;
+      const timeout = setTimeout(() => {
+        setStep((prev) => prev + 1);
+      }, timeoutDuration); 
+      return () => clearTimeout(timeout); 
+    }
+  }, [step]);
+
   return (
     <div className="about-me-container">
       <div className="image-container">
@@ -11,16 +24,21 @@ const AboutMe = () => {
       </div>
 
       <div className="text-container">
-        <h1>Hello!</h1>
-        <h1>My name is Jenna Stover</h1>
-        <h2 id="job-title">{'•'} Front-end Web Developer and Designer {'•'}</h2>
-        <div id="description">I have passion for front-end software engineering, and am currently in my senior year at the 
-          University of South Carolina studying Computer Information Systems. I enjoy taking on challenging
-          tasks and continuously pushing myself when it comes to enhancing my technical skills. I am looking
-          forward to immersing myself into this field post-graduation and putting my skills to use in a professional setting!</div>
+      <h1 className={step > 0 ? 'show' : ''}>Hello!</h1>
+      <h1 className={step > 1 ? 'show' : ''}>My name is Jenna Stover</h1>
+      <h2 id="job-title" className={step > 2 ? 'show' : ''}>
+        {'•'} I'm a Front-end Web Developer and Designer {'•'}
+      </h2>
+      <div id="description" className={step > 3 ? 'show' : ''}>
+        I have a passion for front-end software engineering, and am currently in my senior year at the
+        University of South Carolina studying Computer Information Systems. I enjoy taking on challenging
+        tasks and continuously pushing myself when it comes to enhancing my technical skills. I am looking
+        forward to immersing myself into this field post-graduation and putting my skills to use in a
+        professional setting!
       </div>
     </div>
-  )
-}
+    </div>
+  );
+};
 
 export default AboutMe;
--- my-portfolio/src/components/ContactSkills.jsx
+++ my-portfolio/src/components/ContactSkills.jsx
@@ -4,38 +4,46 @@ import github from '../assets/github-icon.png';
 import gmail from '../assets/gmail-icon.png';
 import react from '../assets/react-logo.png';
 import javascript from '../assets/javascript-logo.png';
-import htmlcss from '../assets/html-css.png';
+import css from '../assets/css-logo.png';
+import html from '../assets/html-logo.png';
 
 const ContactSkills = () => {
   return (
     <div>
-      <div className="section-contact container">
+      <div className="section-contact-skills container">
         <div className="contact-container">
-          <h2 id="contact-header">CONTACT ME</h2>
-          <div className="linkedin contact">
-            <img src={linkedIn} alt="linkedin" />
-            <p><a href="https://www.linkedin.com/in/jenna-stover-usc/">LinkedIn Profile</a></p>
-          </div>
-          <div className="github contact">
-            <img src={github} alt="github" />
-            <p><a href="https://github.com/jenna-stover">GitHub Profile</a></p>
-          </div>
-          <div className="gmail contact">
-            <img src={gmail} alt="gmail" />
-            <p>Jenna.Stover810@gmail.com</p>
+          <h2 id="contact-header">Contact Me!</h2>
+          <div className="contact-list">
+            <div className="linkedin contact">
+              <img src={linkedIn} alt="linkedin" />
+              <p><a href="https://www.linkedin.com/in/jenna-stover-usc/">LinkedIn Profile</a></p>
+            </div>
+            <div className="github contact">
+              <img src={github} alt="github" />
+              <p><a href="https://github.com/jenna-stover">GitHub Profile</a></p>
+            </div>
+            <div className="gmail contact">
+              <img src={gmail} alt="gmail" />
+              <p>Jenna.Stover810@gmail.com</p>
+            </div>
           </div>
         </div>
 
         <div className="skills-container">
-          <h2 id="skills-header">TECHNICAL SKILLS</h2>
-          <div className="skill-one skill">
-            <img src={react} alt="react-logo" />
-          </div>
-          <div className="skill-two skill">
-            <img src={javascript} alt="javascript-logo" />
-          </div>
-          <div className="skill-three skill">
-            <img src={htmlcss} alt="html-css" />
+          <h2 id="skills-header">Technical Skills</h2>
+          <div className="skills-list">
+            <div className="skill-one skill">
+              <img src={react} alt="react-logo" />
+            </div>
+            <div className="skill-two skill">
+              <img src={javascript} alt="javascript-logo" />
+            </div>
+            <div className="skill-three skill">
+              <img src={css} alt="css-logo" />
+            </div>
+            <div className="skill-three skill">
+              <img src={html} alt="html-logo" />
+            </div>
           </div>
         </div>
       </div>
